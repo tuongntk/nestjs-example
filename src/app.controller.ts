f983594b9722df9a10a92fb10cb33f4159f4a2d9
LAPTOP-6HQ2DI71\Acer Predator <khanhtuongnt@gmail.com>
2021-06-20 13:41:10 +0700

    ok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,9 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { formatTo } from './shared/helpers/number-helper';
-import { durationInMinutes, utcNowTimeStamp, utcNow, durationUpToNowInMinutes } from './shared/helpers/time-helper';
+import { durationInMinutes, utcNowTimeStamp, utcNow, durationUpToNowInMinutes, toUtcDate, toLocalDate, toUtcFormat, toLocalFormat, startOfDayUtcNow, startOfDayUtcTimeStamp, getDayOfWeekUtcNow, getDayOfWeekUtcTimeStamp, getHourUtcTimeStamp, getMinuteUtcTimeStamp, addHourUtcTimeStamp, addMinuteUtcTimeStamp, addHourUtcNow, addMinuteUtcNow } from './shared/helpers/time-helper';
 
 import * as moment from "moment";
+import * as momentTz from 'moment-timezone';
 
 @Controller()
 export class AppController {
@@ -27,71 +28,55 @@ export class AppController {
 
     const nowTimeStamp = 1624107577; //Sat Jun 19 2021 19:59:37 GMT+0700
      
-    console.log(`now: ${moment.unix(nowTimeStamp)}`);
-    console.log(`nowTimeStamp: ${moment.unix(nowTimeStamp).unix()}`);
+    // console.log(`now: ${moment.unix(nowTimeStamp)}`);
+    // console.log(`nowTimeStamp: ${moment.unix(nowTimeStamp).unix()}`);
+    // console.log(`now utc: ${moment.unix(nowTimeStamp).utcOffset(tz).utc()}`);
+    // console.log(`nowTimeStamp utc: ${moment(moment.unix(nowTimeStamp).utcOffset(tz).utc()).unix()}`);
 
-    console.log(`now utc: ${moment.unix(nowTimeStamp).utcOffset(tz).utc()}`);
-    console.log(`nowTimeStamp utc: ${moment(moment.unix(nowTimeStamp).utcOffset(tz).utc()).unix()}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`nowTimeStamp utc: ${nowUtcTimeStamp}`);
+    console.log(`nowString utc: ${nowUtc}`);
 
-    // console.log(`nowTimeStamp utc: ${nowTimeStamp}`);
-    // console.log(`nowString utc: ${now}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`toDate utc: ${toUtcDate(nowTimeStamp)} ${toUtcDate(nowTimeStamp).utcOffset()}`);
+    console.log(`toDate timezone: ${toLocalDate(nowTimeStamp, 420)} ${toLocalDate(nowTimeStamp, 420).utcOffset()}`);
 
-    // console.log(`toDate utc: ${toDate(nowTimeStamp)} ${toDate(nowTimeStamp).utcOffset()}`);
-    // console.log(`toDate timezone: ${toDate(nowTimeStamp, 420)} ${toDate(nowTimeStamp, 420).utcOffset()}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`now toFormat utc: ${toUtcFormat(nowTimeStamp)}`);
+    console.log(`now toFormat timezone: ${toLocalFormat(nowTimeStamp, 420)}`);
 
-    // console.log(`now toFormat utc: ${toFormat(nowTimeStamp)}`);
-    // console.log(`now toFormat timezone: ${toFormat(nowTimeStamp, null, 420)}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`toFormat utc: ${toUtcFormat(1624086832)}`);
+    console.log(`toFormat timezone: ${toLocalFormat(1624086832, 420)}`);
 
-    // console.log(`toFormat utc: ${toFormat(1624086832)}`);
-    // console.log(`toFormat timezone: ${toFormat(1624086832, null, 420)}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`startOfDayNow utc: ${startOfDayUtcNow()}`);
+    console.log(`startOfDayNow timezone: ${startOfDayUtcTimeStamp(1624086832)}`);
 
-    // console.log(`startOfDayNow utc: ${startOfDayNow()}`);
-    // console.log(`startOfDayNow timezone: ${startOfDayNow(420)}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`getDayOfWeekNow utc: ${getDayOfWeekUtcNow()}`);
+    console.log(`getDayOfWeekTimeStamp utc June 18, 2021 2:14:05 PM GMT+07:00 : ${getDayOfWeekUtcTimeStamp(1624000445)}`);
 
-    // console.log(`toTimeStamp start of the day utc: ${startOfDayTimeStamp(startTime)}`);
-    // console.log(`toTimeStamp start of the day timezone: ${startOfDayTimeStamp(startTime, 420)}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`getHourTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${getHourUtcTimeStamp(1624112855)}`);
+    console.log(`getMinuteTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${getMinuteUtcTimeStamp(1624112855)}`);
 
-    // console.log(`getDayOfWeekNow utc: ${getDayOfWeekNow()}`);
-    // console.log(`getDayOfWeekNow timezone: ${getDayOfWeekNow(420)}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
+    console.log(`addMHourTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${addHourUtcTimeStamp(1624112855, 2)}`);
+    console.log(`addMinuteTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${addMinuteUtcTimeStamp(1624112855, 5)}`);
 
-    // console.log(`getDayOfWeekTimeStamp utc June 18, 2021 2:14:05 PM GMT+07:00 : ${getDayOfWeekTimeStamp(1624000445)}`);
-    // console.log(`getDayOfWeekTimeStamp timezone June 18, 2021 2:14:05 PM GMT+07:00 : ${getDayOfWeekTimeStamp(1624000445, 420)}`);
+    console.log(`================================================================`);
 
-    // console.log(`================================================================`);
-
-    // console.log(`getHourTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${getHourTimeStamp(1624112855)}`);
-    // console.log(`getHourTimeStamp timezone Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${getHourTimeStamp(1624112855, 420)}`);
-
-    // console.log(`================================================================`);
-
-    // console.log(`getMinuteTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${getMinuteTimeStamp(1624112855)}`);
-    // console.log(`getMinuteTimeStamp timezone Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${getMinuteTimeStamp(1624112855, 420)}`);
-
-    // console.log(`================================================================`);
-
-    // console.log(`addMHourTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${addHourTimeStamp(1624112855, 2)}`);
-    // console.log(`addMinuteTimeStamp utc Date and time (your time zone): Saturday, June 19, 2021 21:27:35 GMT+07:00 : ${addMinuteTimeStamp(1624112855, 5)}`);
-
-    // console.log(`================================================================`);
-
-    // console.log(`addHourNow utc : ${addHourNow(2)}`);
-    // console.log(`addMinuteNow utc : ${addMinuteNow(5)}`);
+    console.log(`addHourNow utc : ${addHourUtcNow(2)}`);
+    console.log(`addMinuteNow utc : ${addMinuteUtcNow(5)}`);
 
     console.log(`================================================================`);
 
--- src/shared/helpers/time-helper.ts
+++ src/shared/helpers/time-helper.ts
@@ -2,7 +2,7 @@ import * as moment from "moment";
 import { roundTo } from "./number-helper";
 
 export const utcNow = (): moment.Moment => moment().utc()
-export const utcNowTimeStamp = (): number => moment().utc().unix()
+export const utcNowTimeStamp = (): number => utcNow().unix()
 
 export const isValidUtcDate = (timeStampUtc: number): boolean => moment.unix(timeStampUtc).utc().isValid()
 export const isValidLocalDate = (timeStamp: number, timeZone: number): boolean => moment.unix(timeStamp).utcOffset(timeZone).isValid()
@@ -31,7 +31,7 @@ export const toLocalDate = (timeStamp: number, timeZone: number = 420): moment.M
 
 export const toUtcFormat = (timeStampUtc: number, format?: string): string => {
     guardValidUtcDate(timeStampUtc)
-    return moment.unix(timeStampUtc).utc().format(format)
+    return toUtcDate(timeStampUtc).format(format)
 }
 
 export const toLocalFormat = (timeStamp: number, timeZone: number = 420, format?: string): string => {
@@ -50,20 +50,20 @@ export const durationInHours = (startTimeStampUtc: number, endTimeStampUtc: numb
 export const durationUpToNowInMinutes = (startTimeStampUtc: number): number => durationInMinutes(startTimeStampUtc, utcNowTimeStamp())
 export const durationUpToNowInHours = (startTimeStampUtc: number): number => durationInHours(startTimeStampUtc, utcNowTimeStamp())
 
-export const startOfDayUtcNow = (): number => moment().utc().startOf('day').unix()
-export const startOfDayUtcTimeStamp = (timeStampUtc: number): number => moment.unix(timeStampUtc).utc().startOf('day').unix()
+export const startOfDayUtcNow = (): number => utcNow().startOf('day').unix()
+export const startOfDayUtcTimeStamp = (timeStampUtc: number): number => toUtcDate(timeStampUtc).startOf('day').unix()
 
-export const getDayOfWeekUtcNow = (): number => moment().utc().day()
-export const getDayOfWeekUtcTimeStamp = (timeStampUtc: number): number => moment.unix(timeStampUtc).utc().day()
+export const getDayOfWeekUtcNow = (): number => utcNow().day()
+export const getDayOfWeekUtcTimeStamp = (timeStampUtc: number): number => toUtcDate(timeStampUtc).day()
 
-export const getHourUtcTimeStamp = (timeStampUtc: number): number => moment.unix(timeStampUtc).utc().hour()
-export const getMinuteUtcTimeStamp = (timeStampUtc: number, timeZone?: number): number => moment.unix(timeStampUtc).utc().minute()
+export const getHourUtcTimeStamp = (timeStampUtc: number): number => toUtcDate(timeStampUtc).hour()
+export const getMinuteUtcTimeStamp = (timeStampUtc: number, timeZone?: number): number => toUtcDate(timeStampUtc).minute()
 
-export const addHourUtcNow = (hours: number = 0): number => moment().utc().add(hours, 'hours').unix()
-export const addMinuteUtcNow = (minutes: number = 0): number => moment().utc().add(minutes, 'minutes').unix()
+export const addHourUtcNow = (hours: number = 0): number => utcNow().add(hours, 'hours').unix()
+export const addMinuteUtcNow = (minutes: number = 0): number => utcNow().add(minutes, 'minutes').unix()
 
-export const addHourUtcTimeStamp = (timeStampUtc: number, hours: number = 0): number => moment.unix(timeStampUtc).utc().add(hours, 'hours').unix()
-export const addMinuteUtcTimeStamp = (timeStampUtc: number, minutes: number = 0): number => moment.unix(timeStampUtc).utc().add(minutes, 'minutes').unix()
+export const addHourUtcTimeStamp = (timeStampUtc: number, hours: number = 0): number => toUtcDate(timeStampUtc).add(hours, 'hours').unix()
+export const addMinuteUtcTimeStamp = (timeStampUtc: number, minutes: number = 0): number => toUtcDate(timeStampUtc).add(minutes, 'minutes').unix()
 
 export function getBlockListOfUtcTimes(startTimeStampUtc: number, endTimeStampUtc: number, interval: number = 30): number[] {
     let startTimeUtc = toUtcDate(startTimeStampUtc)
